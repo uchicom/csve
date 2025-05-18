148369a379ec37f54e0a0afa4739f0df5352ba47
shigeki.uchiyama <shigeki.uchiyama@uchicom.com>
2025-05-18 09:19:16 +0900

    java21化 #15

--- src/main/java/com/uchicom/csve/util/CSVReader.java
+++ src/main/java/com/uchicom/csve/util/CSVReader.java
@@ -37,6 +37,7 @@ public class CSVReader implements Closeable {
 
   StringBuffer columnBuff = new StringBuffer(128);
   private static final int IS_EOF = -1;
+
   /**
    * Creates a new instance of CVSReader
    *
